fdd33ad958f6f9cf352aa0d1a872c3d008299cbc
Milan Hornak <milanfhornak@gmail.com>
2021-08-16 11:02:50 +0200

    Fixed typos in setup

--- python_version/setup.py
+++ python_version/setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='bioinfobox',
     version=0.3,
-    descripton='Bioinformatics toolbox',
+    description='Bioinformatics toolbox',
     classifiers=[
         'Programming Language :: Python :: 3.8',
     ],
     url='https://github.com/Ardashir52/NDBI044/python_version',
-    intall_requires=[
+    install_requires=[
         'biopython',
         'numpy'
     ],
